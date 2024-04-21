9839fb3aa8bfd73006dcba3a55de16abb6e625d1
strongtu <strongtu@tencent.com>
2024-04-22 00:48:40 +0800

    安装和下载模型的脚本

---
+++ install_modles.ps1
@@ -0,0 +1,10 @@
+wget -c -O models/Stable-diffusion/v1-5-pruned-emaonly.safetensors https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors
+wget -c -O models/Stable-diffusion/gameIconInstitute_v30.safetensors https://huggingface.co/aceevanss/GameIconInstitute_mode/resolve/main/gameIconInstitute_v30.safetensors
+wget -c -O models/Stable-diffusion/Toonify_PonyXl.safetensors https://huggingface.co/BetterThanNothing/Toonify_PonyXl/resolve/main/TOONIFY_PONY.safetensors
+wget -c -O models/Stable-diffusion/chilloutmix_NiPrunedFp32Fix.safetensors https://huggingface.co/naonovn/chilloutmix_NiPrunedFp32Fix/resolve/main/chilloutmix_NiPrunedFp32Fix.safetensors
+wget -c -O models/Stable-diffusion/juggernautXL_v8Rundiffusion.safetensors https://huggingface.co/frankjoshua/juggernautXL_v8Rundiffusion/resolve/main/juggernautXL_v8Rundiffusion.safetensors
+
+wget -c -O models/vae/sdxl_vae.safetensors https://huggingface.co/BetterThanNothing/Toonify_PonyXl/resolve/main/sdxl_vae.safetensors
+wget -c -O models/vae/animevae.pt https://huggingface.co/akibanzu/animevae/resolve/main/animevae.pt
+
+
--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTHON=python
+set GIT=git
+set VENV_DIR=%~dp0%venv
+set COMMANDLINE_ARGS=--xformers
 
 call webui.bat
